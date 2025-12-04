e32a744c421aa995fed2faf200a107210398c49b
SamuelPrasetyo <sprasetyo0511@gmail.com>
2025-12-04 22:42:29 +0700

    Feat: like-comments

--- src/Applications/use_case/LikeCommentUseCase.js
+++ src/Applications/use_case/LikeCommentUseCase.js
@@ -4,26 +4,26 @@ class LikeCommentUseCase {
     this._threadRepository = threadRepository;
   }
 
-  // async execute(useCasePayload) {
-  //   const { threadId, commentId, userId } = useCasePayload;
+  async execute(useCasePayload) {
+    const { threadId, commentId, userId } = useCasePayload;
 
-  //   // Verify thread exists
-  //   await this._threadRepository.verifyAvailableThread(threadId);
+    // Verify thread exists
+    await this._threadRepository.verifyAvailableThread(threadId);
 
-  //   // Verify comment exists
-  //   await this._commentRepository.verifyCommentExists(commentId);
+    // Verify comment exists
+    await this._commentRepository.verifyCommentExists(commentId);
 
-  //   // Check if user has already liked the comment
-  //   const isLiked = await this._commentRepository.verifyCommentLike(commentId, userId);
+    // Check if user has already liked the comment
+    const isLiked = await this._commentRepository.verifyCommentLike(commentId, userId);
 
-  //   if (isLiked) {
-  //     // Unlike the comment
-  //     await this._commentRepository.unlikeComment(commentId, userId);
-  //   } else {
-  //     // Like the comment
-  //     await this._commentRepository.likeComment(commentId, userId);
-  //   }
-  // }
+    if (isLiked) {
+      // Unlike the comment
+      await this._commentRepository.unlikeComment(commentId, userId);
+    } else {
+      // Like the comment
+      await this._commentRepository.likeComment(commentId, userId);
+    }
+  }
 }
 
 module.exports = LikeCommentUseCase;
